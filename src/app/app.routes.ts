9091ddeb80b308e3b93941ff6a3fc6648f57f45a
haren.makwana <haren.makwana@radixweb.com>
2024-04-28 23:11:47 +0530

    Added page not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { Routes , RouterModule} from '@angular/router';
 import { FavouriteJobComponent } from './favourite-job/favourite-job.component';
 import { JobDetailComponent } from './job-detail/job-detail.component';
 import { JobsComponent } from './jobs/jobs.component';
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 export const routes: Routes = [
     { path: '', redirectTo : 'jobs', pathMatch:'full'},
     { path: 'favourite-job', component: FavouriteJobComponent },
     { path: 'job-detail/:id', component: JobDetailComponent },
     { path: 'jobs', component: JobsComponent },
-    { path: '**', redirectTo: 'jobs' }
+    { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
